38af11719a5e1c2a7c6ef475645885953cacfd38
zlhunzzing <zlhunzzing@gmail.com>
2020-02-25 13:36:14 +0900

    react router version

---
+++ src/pages/Ranking.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Ranking extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                랭킹
+            </div>
+         );
+    }
+}
+ 
+export default Ranking;
---
+++ src/pages/Signup.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Signup extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                회원가입
+            </div>
+         );
+    }
+}
+ 
+export default Signup;
