effaa9327cb7942f91aaa2ff292de521f3dd3176
zlhunzzing <zlhunzzing@gmail.com>
2020-03-06 00:19:11 +0900

    구현 : 버그 fix 무기와 일치하는 골드 안사라지는것 fix 목적 : fix

--- src/App.js
+++ src/App.js
@@ -754,7 +754,7 @@ class App extends React.Component {
     if (!character.weapon) {
       if (window.confirm('구매하시겠습니까?')) {
         // console.log(character.gold);
-        if (character.gold > item.cost) {
+        if (character.gold >= item.cost) {
           await this.setState(prevState => ({
             character: {
               id: prevState.character.id,
@@ -780,7 +780,7 @@ class App extends React.Component {
         '장착하고 있는 무기를 잃어버립니다. 계속 구매하시겠습니까?'
       )
     ) {
-      if (character.gold > item.cost) {
+      if (character.gold >= item.cost) {
         this.setState(prevState => ({
           character: {
             id: prevState.character.id,
